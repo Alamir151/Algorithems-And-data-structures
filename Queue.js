22511b4e2698e0f682b1b63d0986a0e6ab69d200
programero <alamirhussen05@gmail.com>
2023-04-25 15:35:57 +0200

    implementing Queue

---
+++ Queue.js
@@ -0,0 +1,43 @@
+class Queue{
+    constructor(){
+        this.items=[];
+      
+        this.rear=0;
+        this.length=0;
+    }
+    enuque(item){
+        this.items.push(item);
+        this.length++;
+        
+    }
+    dequeue(){
+        if(this.length===0)return null;
+        this.length--;
+        this.front++;
+        return this.items.shift();
+    }
+    peek(){
+        if(this.length===0) return null;
+        return this.items[this.length-1];
+    }
+    isEmpty(){
+        return this.length===0;
+    }
+    size(){
+        return this.length;
+    }
+    toList(){
+        return this.items;
+    }
+}
+const qu=new Queue();
+qu.enuque(1);
+qu.enuque(2);
+qu.enuque(3);
+console.log(qu.toList());
+console.log(qu.peek());
+console.log(qu.size());
+qu.dequeue()
+
+console.log(qu.toList());
+console.log(qu.size());
